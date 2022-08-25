74b87ba65b0c6b75b218d413e453d73944e441f2
Marco Braccini <marco.braccini@live.it>
2022-08-25 18:00:07 +0200

    To do: final style

--- src/components/sections/CardsSection.vue
+++ src/components/sections/CardsSection.vue
@@ -1,13 +1,19 @@
 <template>
     <section>
-        <div class="container">
-
+        <div class="container up-case">
+            <div class="button alt-btn spec-btn">
+                Current series
+            </div>
 
             <div class="cards-container flex wrap">
                 <DcSingleCard class="card" v-for="(card, cardIndex) in cardsDcComics" :key="cardIndex" :DcThumb="card.thumb" :DcSeries="card.series" />
             </div>
 
-
+            <div class="flex just-sp-ctr">
+                <div class="button alt-btn">
+                Load more
+            </div>
+            </div>
         </div>
     </section>
 </template>
@@ -32,13 +38,28 @@ export default {
 
 <style scoped lang="scss">
     @import '@/style/common.scss';
+    @import '@/style/utility.scss';
+
 
     section {
         background-color: $black-variant;
         color: #ffffff;
     }
 
+    .container {
+        position: relative;
+        padding: 20px;
+    }
+
     .card {
         width: calc(100% / 6);
+        padding: 20px 10px;
+        font-size: 14px;
+    }
+
+    .spec-btn {
+        position: absolute;
+        left: 0;
+        top: -19px;
     }
 </style>
